55b1baa85501307af3992b618cacb54fa7a1ed46
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2019-12-15 22:53:18 +0000

    Improvements of Import

--- build/generated-sources/ap-source-output/entity/Patientdemographic_.java
+++ build/generated-sources/ap-source-output/entity/Patientdemographic_.java
@@ -5,44 +5,30 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T06:35:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-15T22:49:38")
 @StaticMetamodel(Patientdemographic.class)
 public class Patientdemographic_ { 
 
     public static volatile SingularAttribute<Patientdemographic, Date> registrationTime;
     public static volatile SingularAttribute<Patientdemographic, String> patientSex;
     public static volatile SingularAttribute<Patientdemographic, String> patientContactLand01;
-    public static volatile SingularAttribute<Patientdemographic, String> sessionRole;
-    public static volatile SingularAttribute<Patientdemographic, String> foodAllergies;
+    public static volatile SingularAttribute<Patientdemographic, String> patientPassport;
     public static volatile SingularAttribute<Patientdemographic, String> patientEthnicgroup;
     public static volatile SingularAttribute<Patientdemographic, String> patientMaritalstatus;
+    public static volatile SingularAttribute<Patientdemographic, String> recordCancelled;
     public static volatile SingularAttribute<Patientdemographic, String> patientContactLand02;
-    public static volatile SingularAttribute<Patientdemographic, String> patientBloodType;
     public static volatile SingularAttribute<Patientdemographic, String> patientAddressDSdivision;
-    public static volatile SingularAttribute<Patientdemographic, String> sessionUnit;
     public static volatile SingularAttribute<Patientdemographic, Date> demographiclastupdateOn;
     public static volatile SingularAttribute<Patientdemographic, String> patientContactMobile02;
     public static volatile SingularAttribute<Patientdemographic, String> patientContactMobile01;
     public static volatile SingularAttribute<Patientdemographic, Date> registrationDate;
     public static volatile SingularAttribute<Patientdemographic, String> patientAge;
-    public static volatile SingularAttribute<Patientdemographic, String> otherAllergies;
     public static volatile SingularAttribute<Patientdemographic, String> patientReligion;
-    public static volatile SingularAttribute<Patientdemographic, String> patientCitizenship;
-    public static volatile SingularAttribute<Patientdemographic, String> patientAddressDistrict;
-    public static volatile SingularAttribute<Patientdemographic, String> patientDeath;
-    public static volatile SingularAttribute<Patientdemographic, Date> lastUpdate;
-    public static volatile SingularAttribute<Patientdemographic, String> lastUpdateUser;
-    public static volatile SingularAttribute<Patientdemographic, String> patientAddressGNDivision;
-    public static volatile SingularAttribute<Patientdemographic, Date> updateDateTime;
-    public static volatile SingularAttribute<Patientdemographic, String> patientAddressPHMDivision;
-    public static volatile SingularAttribute<Patientdemographic, Date> registeredDateTime;
-    public static volatile SingularAttribute<Patientdemographic, String> patientPassport;
-    public static volatile SingularAttribute<Patientdemographic, String> recordCancelled;
-    public static volatile SingularAttribute<Patientdemographic, String> updateBy;
     public static volatile SingularAttribute<Patientdemographic, String> patientContactEmail;
-    public static volatile SingularAttribute<Patientdemographic, Date> createDate;
+    public static volatile SingularAttribute<Patientdemographic, String> patientCitizenship;
     public static volatile SingularAttribute<Patientdemographic, String> patientNIC;
     public static volatile SingularAttribute<Patientdemographic, String> patientName;
+    public static volatile SingularAttribute<Patientdemographic, String> patientAddressDistrict;
     public static volatile SingularAttribute<Patientdemographic, String> patientID;
     public static volatile SingularAttribute<Patientdemographic, String> patientDrivingLicense;
     public static volatile SingularAttribute<Patientdemographic, String> patientPersonalTitle;
@@ -51,12 +37,9 @@ public class Patientdemographic_ {
     public static volatile SingularAttribute<Patientdemographic, String> patientAddressLine01;
     public static volatile SingularAttribute<Patientdemographic, String> demographiclastupdateBy;
     public static volatile SingularAttribute<Patientdemographic, String> patientAddressLine03;
-    public static volatile SingularAttribute<Patientdemographic, String> drugAllergies;
     public static volatile SingularAttribute<Patientdemographic, Date> demographicinsertedOn;
-    public static volatile SingularAttribute<Patientdemographic, String> createUser;
-    public static volatile SingularAttribute<Patientdemographic, String> user;
-    public static volatile SingularAttribute<Patientdemographic, String> sessionDepartment;
-    public static volatile SingularAttribute<Patientdemographic, String> motherBHT;
-    public static volatile SingularAttribute<Patientdemographic, Integer> printcount;
+    public static volatile SingularAttribute<Patientdemographic, String> patientAddressGNDivision;
+    public static volatile SingularAttribute<Patientdemographic, String> patientAddressPHMDivision;
+    public static volatile SingularAttribute<Patientdemographic, Date> registeredDateTime;
 
 }
--- src/java/entity/Patientdemographic.java
+++ src/java/entity/Patientdemographic.java
@@ -17,6 +17,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -28,54 +29,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "patientdemographic")
 @XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Patientdemographic.findAll", query = "SELECT p FROM Patientdemographic p")
-    , @NamedQuery(name = "Patientdemographic.findByPatientID", query = "SELECT p FROM Patientdemographic p WHERE p.patientID = :patientID")
-    , @NamedQuery(name = "Patientdemographic.findByPatientNIC", query = "SELECT p FROM Patientdemographic p WHERE p.patientNIC = :patientNIC")
-    , @NamedQuery(name = "Patientdemographic.findByPatientPassport", query = "SELECT p FROM Patientdemographic p WHERE p.patientPassport = :patientPassport")
-    , @NamedQuery(name = "Patientdemographic.findByPatientDrivingLicense", query = "SELECT p FROM Patientdemographic p WHERE p.patientDrivingLicense = :patientDrivingLicense")
-    , @NamedQuery(name = "Patientdemographic.findByMotherBHT", query = "SELECT p FROM Patientdemographic p WHERE p.motherBHT = :motherBHT")
-    , @NamedQuery(name = "Patientdemographic.findByPatientPersonalTitle", query = "SELECT p FROM Patientdemographic p WHERE p.patientPersonalTitle = :patientPersonalTitle")
-    , @NamedQuery(name = "Patientdemographic.findByPatientName", query = "SELECT p FROM Patientdemographic p WHERE p.patientName = :patientName")
-    , @NamedQuery(name = "Patientdemographic.findByPatientSex", query = "SELECT p FROM Patientdemographic p WHERE p.patientSex = :patientSex")
-    , @NamedQuery(name = "Patientdemographic.findByPatientDateofbirth", query = "SELECT p FROM Patientdemographic p WHERE p.patientDateofbirth = :patientDateofbirth")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAge", query = "SELECT p FROM Patientdemographic p WHERE p.patientAge = :patientAge")
-    , @NamedQuery(name = "Patientdemographic.findByPatientCitizenship", query = "SELECT p FROM Patientdemographic p WHERE p.patientCitizenship = :patientCitizenship")
-    , @NamedQuery(name = "Patientdemographic.findByPatientEthnicgroup", query = "SELECT p FROM Patientdemographic p WHERE p.patientEthnicgroup = :patientEthnicgroup")
-    , @NamedQuery(name = "Patientdemographic.findByPatientReligion", query = "SELECT p FROM Patientdemographic p WHERE p.patientReligion = :patientReligion")
-    , @NamedQuery(name = "Patientdemographic.findByPatientMaritalstatus", query = "SELECT p FROM Patientdemographic p WHERE p.patientMaritalstatus = :patientMaritalstatus")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressLine01", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressLine01 = :patientAddressLine01")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressLine02", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressLine02 = :patientAddressLine02")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressLine03", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressLine03 = :patientAddressLine03")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressDSdivision", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressDSdivision = :patientAddressDSdivision")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressDistrict", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressDistrict = :patientAddressDistrict")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressPHMDivision", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressPHMDivision = :patientAddressPHMDivision")
-    , @NamedQuery(name = "Patientdemographic.findByPatientAddressGNDivision", query = "SELECT p FROM Patientdemographic p WHERE p.patientAddressGNDivision = :patientAddressGNDivision")
-    , @NamedQuery(name = "Patientdemographic.findByPatientContactLand01", query = "SELECT p FROM Patientdemographic p WHERE p.patientContactLand01 = :patientContactLand01")
-    , @NamedQuery(name = "Patientdemographic.findByPatientContactLand02", query = "SELECT p FROM Patientdemographic p WHERE p.patientContactLand02 = :patientContactLand02")
-    , @NamedQuery(name = "Patientdemographic.findByPatientContactMobile01", query = "SELECT p FROM Patientdemographic p WHERE p.patientContactMobile01 = :patientContactMobile01")
-    , @NamedQuery(name = "Patientdemographic.findByPatientContactMobile02", query = "SELECT p FROM Patientdemographic p WHERE p.patientContactMobile02 = :patientContactMobile02")
-    , @NamedQuery(name = "Patientdemographic.findByPatientContactEmail", query = "SELECT p FROM Patientdemographic p WHERE p.patientContactEmail = :patientContactEmail")
-    , @NamedQuery(name = "Patientdemographic.findByPatientDeath", query = "SELECT p FROM Patientdemographic p WHERE p.patientDeath = :patientDeath")
-    , @NamedQuery(name = "Patientdemographic.findByRegistrationDate", query = "SELECT p FROM Patientdemographic p WHERE p.registrationDate = :registrationDate")
-    , @NamedQuery(name = "Patientdemographic.findByRegistrationTime", query = "SELECT p FROM Patientdemographic p WHERE p.registrationTime = :registrationTime")
-    , @NamedQuery(name = "Patientdemographic.findByUser", query = "SELECT p FROM Patientdemographic p WHERE p.user = :user")
-    , @NamedQuery(name = "Patientdemographic.findByDemographicinsertedOn", query = "SELECT p FROM Patientdemographic p WHERE p.demographicinsertedOn = :demographicinsertedOn")
-    , @NamedQuery(name = "Patientdemographic.findByDemographiclastupdateBy", query = "SELECT p FROM Patientdemographic p WHERE p.demographiclastupdateBy = :demographiclastupdateBy")
-    , @NamedQuery(name = "Patientdemographic.findByDemographiclastupdateOn", query = "SELECT p FROM Patientdemographic p WHERE p.demographiclastupdateOn = :demographiclastupdateOn")
-    , @NamedQuery(name = "Patientdemographic.findByRecordCancelled", query = "SELECT p FROM Patientdemographic p WHERE p.recordCancelled = :recordCancelled")
-    , @NamedQuery(name = "Patientdemographic.findByRegisteredDateTime", query = "SELECT p FROM Patientdemographic p WHERE p.registeredDateTime = :registeredDateTime")
-    , @NamedQuery(name = "Patientdemographic.findByUpdateBy", query = "SELECT p FROM Patientdemographic p WHERE p.updateBy = :updateBy")
-    , @NamedQuery(name = "Patientdemographic.findByUpdateDateTime", query = "SELECT p FROM Patientdemographic p WHERE p.updateDateTime = :updateDateTime")
-    , @NamedQuery(name = "Patientdemographic.findByPatientBloodType", query = "SELECT p FROM Patientdemographic p WHERE p.patientBloodType = :patientBloodType")
-    , @NamedQuery(name = "Patientdemographic.findByLastUpdate", query = "SELECT p FROM Patientdemographic p WHERE p.lastUpdate = :lastUpdate")
-    , @NamedQuery(name = "Patientdemographic.findByLastUpdateUser", query = "SELECT p FROM Patientdemographic p WHERE p.lastUpdateUser = :lastUpdateUser")
-    , @NamedQuery(name = "Patientdemographic.findByCreateDate", query = "SELECT p FROM Patientdemographic p WHERE p.createDate = :createDate")
-    , @NamedQuery(name = "Patientdemographic.findByCreateUser", query = "SELECT p FROM Patientdemographic p WHERE p.createUser = :createUser")
-    , @NamedQuery(name = "Patientdemographic.findBySessionRole", query = "SELECT p FROM Patientdemographic p WHERE p.sessionRole = :sessionRole")
-    , @NamedQuery(name = "Patientdemographic.findBySessionDepartment", query = "SELECT p FROM Patientdemographic p WHERE p.sessionDepartment = :sessionDepartment")
-    , @NamedQuery(name = "Patientdemographic.findBySessionUnit", query = "SELECT p FROM Patientdemographic p WHERE p.sessionUnit = :sessionUnit")
-    , @NamedQuery(name = "Patientdemographic.findByPrintcount", query = "SELECT p FROM Patientdemographic p WHERE p.printcount = :printcount")})
 public class Patientdemographic implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -94,11 +47,6 @@ public class Patientdemographic implements Serializable {
     @Size(max = 10)
     @Column(name = "patientDrivingLicense")
     private String patientDrivingLicense;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 20)
-    @Column(name = "motherBHT")
-    private String motherBHT;
     @Size(max = 10)
     @Column(name = "patientPersonalTitle")
     private String patientPersonalTitle;
@@ -126,6 +74,8 @@ public class Patientdemographic implements Serializable {
     @Size(max = 20)
     @Column(name = "patientMaritalstatus")
     private String patientMaritalstatus;
+    @Transient
+    private String address;
     @Size(max = 50)
     @Column(name = "patientAddressLine01")
     private String patientAddressLine01;
@@ -166,34 +116,12 @@ public class Patientdemographic implements Serializable {
     @Size(max = 200)
     @Column(name = "patientContactEmail")
     private String patientContactEmail;
-    @Size(max = 3)
-    @Column(name = "patientDeath")
-    private String patientDeath;
-    @Lob
-    @Size(max = 65535)
-    @Column(name = "drugAllergies")
-    private String drugAllergies;
-    @Lob
-    @Size(max = 65535)
-    @Column(name = "foodAllergies")
-    private String foodAllergies;
-    @Lob
-    @Size(max = 65535)
-    @Column(name = "otherAllergies")
-    private String otherAllergies;
-    @Basic(optional = false)
-    @NotNull
     @Column(name = "registrationDate")
     @Temporal(TemporalType.DATE)
     private Date registrationDate;
-    @Basic(optional = false)
-    @NotNull
     @Column(name = "registrationTime")
     @Temporal(TemporalType.TIME)
     private Date registrationTime;
-    @Size(max = 50)
-    @Column(name = "user")
-    private String user;
     @Column(name = "demographicinsertedOn")
     @Temporal(TemporalType.TIMESTAMP)
     private Date demographicinsertedOn;
@@ -213,44 +141,6 @@ public class Patientdemographic implements Serializable {
     @Column(name = "registeredDateTime")
     @Temporal(TemporalType.TIMESTAMP)
     private Date registeredDateTime;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 100)
-    @Column(name = "updateBy")
-    private String updateBy;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "updateDateTime")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date updateDateTime;
-    @Size(max = 255)
-    @Column(name = "patientBloodType")
-    private String patientBloodType;
-    @Column(name = "lastUpdate")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastUpdate;
-    @Size(max = 255)
-    @Column(name = "lastUpdateUser")
-    private String lastUpdateUser;
-    @Column(name = "createDate")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
-    @Size(max = 255)
-    @Column(name = "createUser")
-    private String createUser;
-    @Size(max = 10)
-    @Column(name = "sessionRole")
-    private String sessionRole;
-    @Size(max = 10)
-    @Column(name = "sessionDepartment")
-    private String sessionDepartment;
-    @Size(max = 10)
-    @Column(name = "sessionUnit")
-    private String sessionUnit;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "printcount")
-    private int printcount;
 
     public Patientdemographic() {
     }
@@ -259,21 +149,16 @@ public class Patientdemographic implements Serializable {
         this.patientID = patientID;
     }
 
-    public Patientdemographic(String patientID, String motherBHT, String patientAddressPHMDivision, String patientAddressGNDivision, Date registrationDate, Date registrationTime, String recordCancelled, Date registeredDateTime, String updateBy, Date updateDateTime, int printcount) {
-        this.patientID = patientID;
-        this.motherBHT = motherBHT;
-        this.patientAddressPHMDivision = patientAddressPHMDivision;
-        this.patientAddressGNDivision = patientAddressGNDivision;
-        this.registrationDate = registrationDate;
-        this.registrationTime = registrationTime;
-        this.recordCancelled = recordCancelled;
-        this.registeredDateTime = registeredDateTime;
-        this.updateBy = updateBy;
-        this.updateDateTime = updateDateTime;
-        this.printcount = printcount;
-    }
-
+    @Transient
+    Long pidCount=0l;
+    @Transient
+    private Long pid;
+    
+    
+    
+    
     public String getPatientID() {
+        
         return patientID;
     }
 
@@ -303,14 +188,6 @@ public class Patientdemographic implements Serializable {
 
     public void setPatientDrivingLicense(String patientDrivingLicense) {
         this.patientDrivingLicense = patientDrivingLicense;
-    }
-
-    public String getMotherBHT() {
-        return motherBHT;
-    }
-
-    public void setMotherBHT(String motherBHT) {
-        this.motherBHT = motherBHT;
     }
 
     public String getPatientPersonalTitle() {
@@ -481,37 +358,6 @@ public class Patientdemographic implements Serializable {
         this.patientContactEmail = patientContactEmail;
     }
 
-    public String getPatientDeath() {
-        return patientDeath;
-    }
-
-    public void setPatientDeath(String patientDeath) {
-        this.patientDeath = patientDeath;
-    }
-
-    public String getDrugAllergies() {
-        return drugAllergies;
-    }
-
-    public void setDrugAllergies(String drugAllergies) {
-        this.drugAllergies = drugAllergies;
-    }
-
-    public String getFoodAllergies() {
-        return foodAllergies;
-    }
-
-    public void setFoodAllergies(String foodAllergies) {
-        this.foodAllergies = foodAllergies;
-    }
-
-    public String getOtherAllergies() {
-        return otherAllergies;
-    }
-
-    public void setOtherAllergies(String otherAllergies) {
-        this.otherAllergies = otherAllergies;
-    }
 
     public Date getRegistrationDate() {
         return registrationDate;
@@ -529,13 +375,6 @@ public class Patientdemographic implements Serializable {
         this.registrationTime = registrationTime;
     }
 
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
 
     public Date getDemographicinsertedOn() {
         return demographicinsertedOn;
@@ -577,93 +416,6 @@ public class Patientdemographic implements Serializable {
         this.registeredDateTime = registeredDateTime;
     }
 
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateDateTime() {
-        return updateDateTime;
-    }
-
-    public void setUpdateDateTime(Date updateDateTime) {
-        this.updateDateTime = updateDateTime;
-    }
-
-    public String getPatientBloodType() {
-        return patientBloodType;
-    }
-
-    public void setPatientBloodType(String patientBloodType) {
-        this.patientBloodType = patientBloodType;
-    }
-
-    public Date getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Date lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdateUser() {
-        return lastUpdateUser;
-    }
-
-    public void setLastUpdateUser(String lastUpdateUser) {
-        this.lastUpdateUser = lastUpdateUser;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(String createUser) {
-        this.createUser = createUser;
-    }
-
-    public String getSessionRole() {
-        return sessionRole;
-    }
-
-    public void setSessionRole(String sessionRole) {
-        this.sessionRole = sessionRole;
-    }
-
-    public String getSessionDepartment() {
-        return sessionDepartment;
-    }
-
-    public void setSessionDepartment(String sessionDepartment) {
-        this.sessionDepartment = sessionDepartment;
-    }
-
-    public String getSessionUnit() {
-        return sessionUnit;
-    }
-
-    public void setSessionUnit(String sessionUnit) {
-        this.sessionUnit = sessionUnit;
-    }
-
-    public int getPrintcount() {
-        return printcount;
-    }
-
-    public void setPrintcount(int printcount) {
-        this.printcount = printcount;
-    }
 
     @Override
     public int hashCode() {
@@ -689,5 +441,31 @@ public class Patientdemographic implements Serializable {
     public String toString() {
         return "entity.Patientdemographic[ patientID=" + patientID + " ]";
     }
+
+    public Long getPid() {
+        try{
+            pid = Long.parseLong(patientID);
+        }catch(Exception e){
+            pid = pidCount;
+            pidCount++;
+            patientID = pid + "";
+        }
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public String getAddress() {
+        address = patientAddressLine01 + ", " + patientAddressLine02 + ", " + patientAddressLine03 + ", " + patientAddressDSdivision + ", " + patientAddressDistrict + ".";
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
     
 }
